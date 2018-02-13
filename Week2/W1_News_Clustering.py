5153895502c8457d0330c2a846c584491f65228f
Hahnnz <lphion96@naver.com>
2018-02-13 22:36:31 +0900

    add Week1 assignment. Need to add Explaining

---
+++ Week2/W1_News_Clustering.py
@@ -0,0 +1,100 @@
+
+# coding: utf-8
+
+# # Week 1. 뉴스 클러스터링
+
+# In[1]:
+
+
+import argparse
+
+
+# 입력받는 문장 두개를 원소화해주기 위한 함수
+
+# In[2]:
+
+
+def check_alphabets(ss):
+    # 알파벳 외 문자 확인
+    count=0
+    NotAlpha=[]
+    for i in range(len(ss)):
+        if not ss[i].isalpha():
+            NotAlpha.append(i)
+    # 알파벳 외 문자 제거 
+    for i in range(len(NotAlpha)):
+        ss=ss[0:NotAlpha[i]]+ss[NotAlpha[i]+1:]
+        for j in range(len(NotAlpha)): NotAlpha[j]-=1
+    # 대문자->소문자
+    return ss.lower()
+
+
+# 주어진 문장을 쪼개줌
+
+# In[3]:
+
+
+def split_sentences(s1,s2):
+    s1=check_alphabets(s1)
+    s2=check_alphabets(s2)
+    # 쪼개기
+    sentence1=[]
+    sentence2=[]
+    for i in range(len(s1)-1): sentence1.insert(i,s1[i:i+2])
+    for i in range(len(s2)-1): sentence2.insert(i,s2[i:i+2])
+    return sentence1,sentence2
+
+
+# In[4]:
+
+
+def check_overlapping(letters,collection):
+    for i in range(len(collection)):
+        if letters==collection[i]:
+            return True
+
+
+# 점수
+
+# In[5]:
+
+
+def Scoring(s1,s2):
+    intersection=[]
+    Union=[]
+    for i in range(len(s1)):
+        for j in range(len(s2)):
+            if s1[i]==s2[j] :
+                if not check_overlapping(s1[i],intersection): intersection.append(s1[i])
+                if not check_overlapping(s1[i],Union): Union.append(s1[i])
+            elif s1[i]!=s2[j]:
+                if not check_overlapping(s1[i],Union): Union.append(s1[i])
+                if not check_overlapping(s2[i],Union): Union.append(s2[i])
+    return int(len(intersection)/len(Union)*65536)
+
+
+# In[6]:
+
+
+def get_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("stc1",type=str,help="비교할 첫번째 단어를 입력해주세요. (문자열의 길이는 2 이상, 1,000 이하)")
+    parser.add_argument("stc2",type=str,help="비교할 두번째 단어를 주세요. (문자열의 길이는 2 이상, 1,000 이하)")
+    
+    args = parser.parse_args()
+
+    stc1 = args.stc1
+    stc2 = args.stc2
+
+    return stc1,stc2
+
+
+# In[7]:
+
+
+# 메인 함수
+if __name__=="__main__":
+    stc1,stc2=get_argument()
+    stc1,stc2=split_sentences(stc1,stc2)
+    print (Scoring(stc1,stc2))
+
